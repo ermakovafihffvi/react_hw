0a99d0dc23c5cfe1b49af69eb5ce6427fa308ee0
ermakovafihffvi <veraermakova.fihffvi@gmail.com>
2022-06-12 20:12:27 +0300

    спустя долю секуды после нажатия на кнопку состояние возвращается к исходному - не смогла продвинуться дальше из-за этого

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import './css/App.css';
+import React from 'react';
 
 function App() {
+  const [ messageList, setMessageList ] = React.useState([
+    {
+      text: "ldkndskfsnd",
+      author: "x,v,dsvv,n"
+    }
+  ]);
+
+  const updateMessages = () => {
+    setMessageList([
+      ...messageList, 
+      {
+        text: "new message",
+        author: "me"
+      }
+    ]);
+  }
+
+  const result = messageList.map(message => {
+    return (
+      <li key={message.author}>
+        <h3>Author: {message.author}</h3>
+        <p>Text: {message.text}</p>
+      </li>
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {result}
+      </ul>
+
+      <form onSubmit={updateMessages}>
+        <input name="text" type="text"></input>
+        <button type="submit">Отправить</button>
+      </form>
     </div>
   );
+
+  /*const [counter, setCounter] = React.useState(0);
+  const increase = () => {
+    setCounter(counter => counter+1)
+  }
+  return(
+    <div className='App'>    
+      <button onClick={increase}>+</button>
+      <p>{counter}</p>
+    </div>
+  );*/
 }
 
 export default App;
