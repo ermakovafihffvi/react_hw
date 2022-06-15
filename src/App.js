5f8cf359af6b385791469e8a1eda8e41a048ed2c
ermakovafihffvi <veraermakova.fihffvi@gmail.com>
2022-06-15 21:40:18 +0300

    доделана 2 домашка (после урока)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 //import logo from './logo.svg';
 import './css/App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
-  const [ messageList, setMessageList ] = React.useState([
-    {
-      text: "ldkndskfsnd",
-      author: "x,v,dsvv,n"
-    }
-  ]);
+  const [ messageList, setMessageList ] = React.useState([]);
+  const [ text, setText ] = React.useState('');
+  const [ author, setAuthor ] = React.useState('');
 
-  const updateMessages = () => {
-    setMessageList([
-      ...messageList, 
+  useEffect(() => {
+    setTimeout(() => {
+      botAnswer()
+    }, 2000)
+  }, [messageList]);
+
+  function getId(array){
+    return array.length ? array[array.length - 1].id + 1 : 0;    
+  }
+
+  function botAnswer(){
+    let lastAuthor = messageList[messageList.length - 1].author;
+    if(lastAuthor){
+      setMessageList(prevState => [
+        ...prevState,
+        {
+          id: getId(prevState),
+          text: 'сообщение отправлено'
+        }
+      ]);
+    }
+  }
+
+  const updateMessages = (e) => {
+    e.preventDefault();
+    setMessageList(prevState => [
+      ...prevState, 
       {
-        text: "new message",
-        author: "me"
+        id: getId(prevState),
+        text,
+        author
       }
     ]);
   }
 
   const result = messageList.map(message => {
     return (
-      <li key={message.author}>
+      <li key={message.id}>
         <h3>Author: {message.author}</h3>
         <p>Text: {message.text}</p>
       </li>
@@ -36,22 +58,18 @@ function App() {
       </ul>
 
       <form onSubmit={updateMessages}>
-        <input name="text" type="text"></input>
+        <div>
+          <p>Текст сообщения:</p>
+          <input value={text} onChange={(e) => setText(e.target.value)} type="text"></input>
+        </div>
+        <div>
+          <p>Автор:</p>
+          <input value={author} onChange={(e) => setAuthor(e.target.value)} type="text"></input>
+        </div>
         <button type="submit">Отправить</button>
       </form>
     </div>
   );
-
-  /*const [counter, setCounter] = React.useState(0);
-  const increase = () => {
-    setCounter(counter => counter+1)
-  }
-  return(
-    <div className='App'>    
-      <button onClick={increase}>+</button>
-      <p>{counter}</p>
-    </div>
-  );*/
 }
 
 export default App;
